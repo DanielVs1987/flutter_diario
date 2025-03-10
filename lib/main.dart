329ef7f537cac8c7e35ad3bd0ca29704e6eb57b6
DanielVs1987 <daniel.valverde.silva.1987@gmail.com>
2025-03-10 20:02:07 -0300

    Inicializa o projeto com estrutura base

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'models/journal.dart';
+import 'screens/add_journal_screen/add_journal_screen.dart';
+import 'screens/home_screen/home_screen.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (routeSettings) {
+
+        if (routeSettings.name == "add-journal") {
+          Map<String, dynamic> map = routeSettings.arguments as Map<String, dynamic>;
+          final Journal journal = map["journal"] as Journal;
+          final bool isEditing = map["is_editing"];
+
+          return MaterialPageRoute(
+            builder: (context) {
+              return AddJournalScreen(journal: journal, isEditing: isEditing,);
+            },
+          );
+        }
+        return null;
+      },
+    );
+  }
+}
